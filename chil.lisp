4df2d19b4b4373fac3ee4fd8e8d0ea3406f748ad
Karl Hallsby <karl@hallsby.com>
2023-05-03 21:27:22 -0500

    Create structure that defines a module
    Currently, this structure only contains the inputs to the module and the outputs
    from this module.

--- chil.lisp
+++ chil.lisp
@@ -4,5 +4,14 @@
 
 (in-package :chil)
 
+(defstruct (module
+            (:constructor module (name &key (inputs '()) (outputs '()))))
+  (name "" :type string)
+  ;; FIXME: inputs/outputs should be set, not list. We want to prevent people
+  ;; from using the same symbol in the input/output collections.
+  ;; TODO: But we DO allow the same object to be present in both.
+  (inputs '() :type list)
+  (outputs '() :type list))
+
 (defun hello ()
   (print "Hello world, from CHIL."))
