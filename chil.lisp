f682ed4fb05e0973d17928a603c0fe38ee59f6da
Karl Hallsby <karl@hallsby.com>
2023-05-10 05:07:23 -0500

    Move Verilog generator to new generator class & methods

--- chil.lisp
+++ chil.lisp
@@ -8,29 +8,6 @@
 
 (in-package :chil)
 
-(defun generate-verilog-module-args (direction args)
-  "Generate Verilog argument string based on ARGS, noting the DIRECTION of the
-arguments in the string as well.
-
-The two directions supported are the symbols 'input and 'output."
-  (defun arg->string (arg)
-    (format nil "~a type ~a,~&" (string-downcase (symbol-name direction)) arg))
-
-  (uiop:reduce/strcat
-   (mapcar #'arg->string args)))
-
-(defun generate-verilog (module)
-  "Generate the Verilog corresponding to the provided Chil MODULE."
-  (uiop:strcat
-   (format nil "module ~a" (module-name module))
-   (format nil "(~&")
-   (generate-module-args 'input (module-inputs module))
-   (generate-module-args 'output (module-outputs module))
-   (format nil ")~&")
-   ;; TODO: Get indentation working
-   (format nil "body;~&")
-   (format nil "endmodule // ~a" (module-name module))))
-
 (defun write-out-module-verilog (module &optional file-name)
   "Generate the lower-level representation of MODULE and write to disk.
 
@@ -40,7 +17,10 @@ is named \"module-name.v\"."
                        :direction :output
                        :if-exists :supersede
                        :if-does-not-exist :create)
-    (write-string (generate-verilog module) out)))
+    (write-string (generate (make-instance 'verilog-generator) module) out)))
 
 (defun hello ()
-  (print "Hello world, from CHIL."))
+  (let ((m (module "test"
+                   :inputs '("addr" "ready")
+                   :outputs '("data" "valid"))))
+    (generate (make-instance 'verilog-generator) m)))
--- generator.lisp
+++ generator.lisp
@@ -9,3 +9,33 @@
 
 (defgeneric generate (generator module)
   (:documentation "Abstract method to generate the provided CHIL MODULE as another HDL."))
+
+
+;;; Verilog Generator
+(defclass verilog-generator (hdl-generator)
+  ())
+
+(defmethod generate-module-args ((generator verilog-generator) direction args)
+  "Generate Verilog argument string based on ARGS, noting the DIRECTION of the
+arguments in the string as well.
+
+The two directions supported are the symbols 'input and 'output."
+  (defun arg->string (arg)
+    (format nil "~a type ~a,~&" (string-downcase (symbol-name direction)) arg))
+
+  (uiop:reduce/strcat
+   (mapcar #'arg->string args)))
+
+(defmethod generate ((generator verilog-generator) module)
+  "Generate Verilog for the provided MODULE."
+  (uiop:strcat
+   (format nil "module ~a" (chil:module-name module))
+   (format nil "(~&")
+   (generate-module-args generator 'input (chil:module-inputs module))
+   (generate-module-args generator 'output (chil:module-outputs module))
+   (format nil ")~&")
+   ;; TODO: Actually write a real body
+   ;; TODO: Get indentation working
+   (format nil "body;~&")
+   (format nil "endmodule // ~a" (chil:module-name module))))
+
