94707822e5ec2536500ef5aa7703fe89293b6ee7
Karl Hallsby <karl@hallsby.com>
2023-05-17 05:59:53 -0500

    Use equal for string comparison in tests
    eql performs what amounts to pointer comparison, whereas equal enters the
    object and compares them structurally.
    
    Fix indentation

--- tests/write-verilog.lisp
+++ tests/write-verilog.lisp
@@ -24,32 +24,32 @@
   (chil:generate (make-instance 'chil:verilog-generator) module))
 
 (define-test generate-verilog-empty-module ()
-  (assert-eql "module test-empty(
+  (assert-equal "module test-empty(
 )
 body;
 endmodule // test-empty"
-              (generate-verilog *test-module-empty*)))
+                (generate-verilog *test-module-empty*)))
 
 (define-test generate-verilog-module-only-inputs ()
-  (assert-eql "module test-inputs(
+  (assert-equal "module test-inputs(
 input type addr,
 input type ready,
 )
 body;
 endmodule // test-inputs"
-              (generate-verilog *test-module-inputs*)))
+                (generate-verilog *test-module-inputs*)))
 
 (define-test generate-verilog-module-only-outputs ()
-  (assert-eql "module test-outputs(
+  (assert-equal "module test-outputs(
 output type addr,
 output type valid,
 )
 body;
 endmodule // test-outputs"
-              (generate-verilog *test-module-outputs*)))
+                (generate-verilog *test-module-outputs*)))
 
 (define-test generate-verilog-module-inputs-outputs ()
-  (assert-eql "module test-inputs-outputs(
+  (assert-equal "module test-inputs-outputs(
 input type addr,
 input type valid,
 output type addr,
@@ -57,4 +57,4 @@ output type valid,
 )
 body;
 endmodule // test-inputs-outputs"
-              (generate-verilog *test-module-inputs-outputs*)))
+                (generate-verilog *test-module-inputs-outputs*)))
