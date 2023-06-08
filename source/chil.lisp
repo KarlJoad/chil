8f03a81650754273b957d7db78fdae1db7b5df80
Karl Hallsby <karl@hallsby.com>
2023-06-08 09:59:36 -0500

    Keep top-level chil file with no dependencies

--- source/chil.lisp
+++ source/chil.lisp
@@ -19,19 +19,5 @@
 
 (in-package :chil)
 
-(defun write-out-module-verilog (module &optional file-name)
-  "Generate the lower-level representation of MODULE and write to disk.
-
-You may optionally provide a name for the generated file. By default, this file
-is named \"module-name.v\"."
-  (with-open-file (out file-name
-                       :direction :output
-                       :if-exists :supersede
-                       :if-does-not-exist :create)
-    (generate (make-instance 'verilog-generator) module out)))
-
 (defun hello ()
-  (let ((m (module "test"
-                   :inputs '("addr" "ready")
-                   :outputs '("data" "valid"))))
-    (generate (make-instance 'verilog-generator) m nil)))
+  (format t "Hello to Chil!~&"))
