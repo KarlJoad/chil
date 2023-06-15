ce1bbf1729d1cb186f3736738ae16b7075381c7a
Karl Hallsby <karl@hallsby.com>
2023-06-15 12:44:04 -0500

    Fix log2up to accept 0 as valid input, returning 1

--- source/utils.lisp
+++ source/utils.lisp
@@ -8,4 +8,5 @@
   "Compute the number of bits required to represent VAL in binary."
   (assert (and (integerp val)
                (not (minusp val))))
-  (1+ (values (ceiling (log val 2)))))
+  (max 1 ;; If val = 0, then the next expr |- 0, but we need 1 bit to represent 0
+       (values (ceiling (log (1+ val) 2)))))
