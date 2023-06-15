a13b72da8e3447d9d110ee7a8a81a3ea1ea056c5
Karl Hallsby <karl@hallsby.com>
2023-06-15 12:51:30 -0500

    Chil utils log2up util tests now run and are correct
    These are the kind of tests that I would want to have automated through a
    property-based testing system.
    
    Need 1 bit to represent 0.

--- tests/utils.lisp
+++ tests/utils.lisp
@@ -1,21 +1,15 @@
 (in-package :chil/tests)
 
 (define-test log2up-of-zero ()
-  (assert-equal 0 (chil:log2up 0)))
+  (assert-eql 1 (chil/utils:log2up 0)))
 
 (define-test log2up-of-negative ()
   ;; This test SHOULD throw an error
-  (assert-equal "module test-outputs(
-output type addr,
-output type valid,
-)
-body;
-endmodule // test-outputs"
-                (chil:log2up -1)))
+  (assert-error 'simple-error (chil/utils:log2up -1)))
 
 (define-test log2up-normal ()
-  (assert-equal 1 (chil:log2up 1))
-  (assert-equal 2 (chil:log2up 2))
-  (assert-equal 8 (chil:log2up 127))
-  (assert-equal 8 (chil:log2up 128))
-  (assert-equal 9 (chil:log2up 129)))
+  (assert-equal 1 (chil/utils:log2up 1))
+  (assert-equal 2 (chil/utils:log2up 2))
+  (assert-equal 8 (chil/utils:log2up 127))
+  (assert-equal 8 (chil/utils:log2up 128))
+  (assert-equal 9 (chil/utils:log2up 129)))
