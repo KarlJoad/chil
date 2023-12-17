114bace19a2a00f76bb722a863c5aca7137d9692
Karl Hallsby <karl@hallsby.com>
2023-12-16 18:39:39 -0600

    Add check-it example for a test which fails

--- tests/example.lisp
+++ tests/example.lisp
@@ -12,3 +12,13 @@
   (assert-true
    (check-it:check-it (check-it:generator (check-it:guard #'evenp (integer)))
                       (lambda (x) (evenp x)))))
+
+;; The test below will not always be correct, because there are generated cases
+;; where the input will produce an incorrect output.
+;; check-it:check-it returns 'nil (which is false-y) when there is even ONE test
+;; case that is incorrect. assert-false means we catch that as an "expected"
+;; error.
+(define-test check-it-example-failing ()
+  (assert-false
+   (check-it:check-it (check-it:generator (integer))
+                      (lambda (x) (evenp x)))))
